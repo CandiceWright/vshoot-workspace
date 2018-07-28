a87925f3381e5c6b97985cece2e8f56d89b484c9
Candice <candicewright@candices-macbook-air-4.local>
2018-07-28 11:52:36 -0700

    Initial Commit

---
+++ VShootApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VShootApplication
+//
+//  Created by Princess Candice on 7/28/18.
+//  Copyright © 2018 Candice Wright. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
